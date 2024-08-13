8194e8c87d4122013b9d9879161de04987fd7994
Mohamed Amir <83864018+Mohamed22198@users.noreply.github.com>
2024-08-13 14:28:49 +0300

    Use MongoAtlas

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use("/seller", sellerRoute);
 app.use(express.static(path.join(__dirname, "client")));
 
 mongoose
-  .connect("mongodb://localhost:27017/market", {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
